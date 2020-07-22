eba50804ee8caaee56cc580935d8d138867227ed
Marten/Qqwy <w-m@wmcode.nl>
2020-07-22 20:10:35 +0200

    Extra doctest

--- lib/capture_pipe.ex
+++ lib/capture_pipe.ex
@@ -1,22 +1,38 @@
 defmodule CapturePipe do
-  @doc """
-  A pipe-operator that extends the normal pipe
-  in one tiny way:
-  It allows the syntax of having a bare `&1` capture
-  to exist inside a datastructure as one of the pipe results.
-  This is useful to insert the pipe's results into a datastructure
-  such as a tuple.
-  What this pipe-macro does, is if it encounters a `&` capture,
-  it wraps the whole operand in `((...)).()` which is the
-  anonymous-function-call syntax that the Kernel pipe accepts,
-  that (argubably) is much less easy on the eyes.
-  So `10 |> &{:ok, &1}` is turned into `10 |> (&({:ok, &1})).()`
+  import Kernel, except: [|>: 2]
+  @moduledoc """
   To use this operator in one of your modules, you need to add the following to it:
-  
+
+      use CapturePipe
+
+  This does the same thing as explicitly writing:
+
       import Capturepipe
       import Kernel, except: [|>: 2]
-  
+  """
+
+  defmacro __using__(_opts) do
+    quote do
+      import CapturePipe
+      import Kernel, except: [|>: 2]
+    end
+  end
+
+  @doc """
+  Extended pipe-operator that allows the usage of bare function captures.
+
+  This is useful to insert the pipe's results into a datastructure
+  such as a tuple.
+
+  What this macro does, is if it encounters a `&` capture,
+  it wraps the whole operand in `(...).()` which is the
+  anonymous-function-call syntax that Elixir's normal pipe accepts,
+  that (argubably) is less easy on the eyes.
+
+  For instance, `10 |> &{:ok, &1}` is turned into `10 |> (&{:ok, &1}).()`
+
   ## Examples
+
   Still works as normal:
 
       iex> [1,2,3] |> Enum.map(fn x -> x + 1 end)
@@ -31,33 +47,66 @@ defmodule CapturePipe do
 
       iex> 20 |> &{:ok, &1} |> &[&1, 2, 3]
       [{:ok, 20}, 2, 3]
+
+  Even if the pipes are nested deeply
+  and interspersed with 'normal' pipe calls:
+
+      iex> (10
+      iex> |> &Kernel.div(20, &1)
+      iex> |> Kernel.-()
+      iex> |> to_string()
+      iex> |> &"The answer is: \#{&1}!"
+      iex> |> String.upcase()
+      iex> |> &{:ok, &1}
+      iex> )
+      {:ok, "THE ANSWER IS: -2!"}
   """
-  defmacro prev |> ({:&, meta, [{:|>, _, [pipe_lhs, pipe_rhs]}]}) do
-    # Prevents pipe being wrongly nested insice `&`
-    lhs_capture = {:&, meta, [pipe_lhs]}
-    quote do
-      unquote(prev)
-      |> unquote(wrap_if_capture(lhs_capture))
-      |> unquote(wrap_if_capture(pipe_rhs))
+  # The implementation is identical to
+  # Elixir's builtin one,
+  # except that we are using our overridden
+  # `pipe` and `unpipe` functions.
+  # (rather than the ones in the `Macro` module)
+  defmacro left |> right do
+    [{h, _} | t] = unpipe({:|>, [], [left, right]})
+
+    fun = fn {x, pos}, acc ->
+      pipe(acc, x, pos)
     end
+
+    :lists.foldl(fun, h, t)
   end
 
-  defmacro prev |> (capture = {:&, _, _}) do
-    prev = Macro.expand(prev, __CALLER__)
-    quote do
-      Kernel.|>(unquote(prev), (unquote(capture)).())
-    end
-  end
-  defmacro prev |> other do
-    quote do
-      Kernel.|>(unquote(prev), unquote(other))
-    end
+  # Implementation is _very_ similar to Elixir's builtin
+  # `Macro.unpipe/1`
+  defp unpipe(expr) do
+    :lists.reverse(unpipe(expr, []))
   end
 
-  defp wrap_if_capture(capture = {:&, _, _}) do
-    quote do
-      (unquote(capture)).()
-    end
+  defp unpipe({:|>, _, [left, right]}, acc) do
+    unpipe(right, unpipe(left, acc))
   end
-  defp wrap_if_capture(other), do: other
+
+  # The magic happens here:
+  # We rewrite `a |> (&b |> c)`
+  # into `a |> (&b) |> c`
+  # and then recurse
+  defp unpipe({:&, meta1, [{:|>, meta2, [left, right]}]}, acc) do
+    unpipe({:|>, meta2, [{:&, meta1, [left]}, right]}, acc)
+  end
+
+  defp unpipe(other, acc) do
+    [{other, 0} | acc]
+  end
+
+  # Bare captures are wrapped to become
+  # (&(...)).()
+  defp pipe(expr, {:&, _, _} = call_args, position) do
+    pipe(expr, quote do (unquote(call_args)).() end, position)
+  end
+
+  # Defer the rest of the implementation
+  # to the one that already ships with Elixir
+  defp pipe(expr, other, position) do
+    Macro.pipe(expr, other, position)
+  end
 end
--- test/capture_pipe_test.exs
+++ test/capture_pipe_test.exs
@@ -4,4 +4,31 @@ defmodule CapturePipeTest do
   import CapturePipe
   import Kernel, except: [|>: 2]
   doctest CapturePipe
+
+
+  describe "More complicated sequences of pipes with captures" do
+    def speak(name, greeting) do
+      name
+      # * function that requires different order
+      |> &greet(greeting, &1)
+      # regular function still works fine
+      |> String.upcase()
+      # does not disrupt adding ellipses via original method
+      |> (&(&1 <> "...")).()
+      # * example of adding ellipses with enhanced pipe
+      # could also be &("..." <> &1) if you prefer
+      |> &"...#{&1}"
+      # * returning a tuple
+      |> &{:ok, &1}
+    end
+
+    defp greet(greeting, name) do
+      "#{greeting}, #{name}"
+    end
+
+    test "capture pipe usage" do
+      assert speak("Jane", "Hello") == {:ok, "...HELLO, JANE..."}
+    end
+  end
+
 end
